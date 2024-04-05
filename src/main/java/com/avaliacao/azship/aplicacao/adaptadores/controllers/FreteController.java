8579b4f080d8211147fbcaadea958ccf83e94be4
mariamacedo2xx@gmail.com <mariamacedo2xx@gmail.com>
2024-04-05 09:49:27 -0300

    Primeira chamada no controller frete

---
+++ src/main/java/com/avaliacao/azship/aplicacao/adaptadores/controllers/FreteController.java
@@ -0,0 +1,15 @@
+package com.avaliacao.azship.aplicacao.adaptadores.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("fretes")
+public class FreteController {
+
+	@GetMapping
+    String getProdutos() {
+        return "Hello world";
+    }
+}
